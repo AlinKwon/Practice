10ee2d46470b72480b6f205aca650de66a859d82
Alin.Kwon <jonghoon@shinsegae.com>
2023-08-04 17:30:49 +0900

    components

---
+++ BlazorHybrid/2.ComponentsReferenceTemplate/Blazor.Components/BaseComponents/BaseCounterComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Components.BaseComponents
+{
+	public class BaseCounterComponent : ComponentBase
+	{
+		[Parameter]
+        public int Count { get; set; }
+	}
+}
---
+++ BlazorHybrid/2.ComponentsReferenceTemplate/Blazor.WPF/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+
+namespace Blazor.WPF
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddBlazorWebViewDeveloperTools();
+
+
+			Resources.Add("services", serviceCollection.BuildServiceProvider());
+
+			InitializeComponent();
+		}
+	}
+}
