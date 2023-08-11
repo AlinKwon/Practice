784cf2f04d4d3ecf52ed408d5f038426d00a74b9
Alin.Kwon <jonghoon@shinsegae.com>
2023-08-11 11:22:19 +0900

    theme

---
+++ BlazorHybrid/3.ThemeTemplate/Blazor.Components/BaseComponent/ThemeComponent.cs
@@ -0,0 +1,21 @@
+﻿using Blazor.Components.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Components.BaseComponent;
+
+public class ThemeComponent : ComponentBase
+{
+	[Parameter]
+	public int? Width { get; set; } = null;
+
+	[Parameter]
+	public int? Height { get; set; } = null;
+
+	[CascadingParameter]
+	public ThemeInfo? ThemeInfo { get; set; } = null;
+}
